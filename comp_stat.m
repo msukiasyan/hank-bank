0cba4d9834b76e41bb80e3b9f8826bf9edc67587
mikae <mikae@10.150.208.140>
2019-04-21 11:05:58 -0500

    Minor change to the initial interest rates guesses to improve stability.

--- comp_stat.m
+++ comp_stat.m
@@ -2,13 +2,14 @@ function cs_res = comp_stat(opt, p, par, low, high, npoints)
     cs_grid                 = linspace(low, high, npoints);
     cs_sol                  = cell(npoints, 1);
     cs_stats                = cell(npoints, 1);
-
+    rates_old               = [0.038 0.04];
     for piter = 1:npoints
         p.(par)             = cs_grid(piter);
         
         rates               = fmincon(@(x) eqs(opt, p, x), ...
-        [0.03 0.035], [1, -1], 0, [], [], [0.0 0.0], [0.1 0.1], [], ...
-        optimoptions('fmincon', 'Display', 'iter'));
+                                rates_old, [1, -1], 0, [], [], [0.0 0.0], [0.1 0.1], [], ...
+                                optimoptions('fmincon', 'Display', 'iter'));
+        rates_old           = rates;
 
         p.r_F               = rates(2);
         p.r_minus           = rates(1);
